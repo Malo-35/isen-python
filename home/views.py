a4cf0eb9fd1e0774002ff51c1f5a2b16e33b9437
Malo-35 <malo.barbaud@isen.yncrea.fr>
2025-04-25 14:43:46 +0200

    html&views: remplacement de views et ajout dans home

--- home/views.py
+++ home/views.py
@@ -1,21 +1,18 @@
-from django.shortcuts import redirect
 from django.views.generic import ListView
-
 from products.models import Product
-
-
-def RedirectHomeView(request):
-    '''
-    Redirect url from '/' to '/home/'
-    '''
-    return redirect('home')
 
 class HomeView(ListView):
     '''
-    Renders home page with all the products
+    Renders home page with all the products, with optional price sorting
     '''
     template_name = 'home.html'
     model = Product
+    context_object_name = 'object_list'  # important pour le template
 
-    def get(self, request):
-        return super().get(request)
+    def get_queryset(self):
+        sort = self.request.GET.get('sort')
+        if sort == 'price_asc':
+            return Product.objects.all().order_by('price')
+        elif sort == 'price_desc':
+            return Product.objects.all().order_by('-price')
+        return Product.objects.all()
