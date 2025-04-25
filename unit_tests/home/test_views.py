4300e77447f71997886cd8fe717d49028d5cfebd
Malo-35 <malo.barbaud@isen.yncrea.fr>
2025-04-25 16:23:48 +0200

    Ajout des test unitaires.

--- unit_tests/home/test_views.py
+++ unit_tests/home/test_views.py
@@ -1,9 +1,10 @@
 from django.urls import reverse 
-from django.test import Client
+from django.test import Client, RequestFactory, TestCase
 from pytest_django.asserts import assertTemplateUsed
 import pytest
 
 from home.views import RedirectHomeView, HomeView
+from products.models import Product
 
 
 def test_RedirectHomeView():
@@ -30,3 +31,50 @@ def test_HomeView():
     
     assert response.status_code == 200
     assertTemplateUsed(response, 'home.html')
+
+class HomeViewTestCase(TestCase):
+    def setUp(self):
+        # Crée un ensemble de produits
+        Product.objects.create(name='Produit A', price=10.00, description="Desc", image="img.png")
+        Product.objects.create(name='Produit B', price=20.00, description="Desc", image="img.png")
+        Product.objects.create(name='Produit C', price=15.00, description="Desc", image="img.png")
+
+        self.factory = RequestFactory()
+
+    def test_get_queryset_default_order(self):
+        request = self.factory.get('/home/')
+        view = HomeView()
+        view.request = request
+
+        queryset = view.get_queryset()
+        prices = list(queryset.values_list('price', flat=True))
+        # Aucun tri appliqué, donc dépend de l'ordre d'insertion
+        self.assertEqual(len(prices), 3)
+
+    def test_get_queryset_price_asc(self):
+        request = self.factory.get('/home/?sort=price_asc')
+        view = HomeView()
+        view.request = request
+
+        queryset = view.get_queryset()
+        prices = list(queryset.values_list('price', flat=True))
+        self.assertEqual(prices, sorted(prices))
+
+    def test_get_queryset_price_desc(self):
+        request = self.factory.get('/home/?sort=price_desc')
+        view = HomeView()
+        view.request = request
+
+        queryset = view.get_queryset()
+        prices = list(queryset.values_list('price', flat=True))
+        self.assertEqual(prices, sorted(prices, reverse=True))
+
+    def test_get_queryset_invalid_sort(self):
+        request = self.factory.get('/home/?sort=invalid')
+        view = HomeView()
+        view.request = request
+
+        queryset = view.get_queryset()
+        prices = list(queryset.values_list('price', flat=True))
+        # Devrait ne pas trier, donc on vérifie juste que les éléments sont là
+        self.assertEqual(len(prices), 3)
