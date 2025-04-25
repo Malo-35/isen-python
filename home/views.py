c815480200f5963c4c621036837a725ae3d6ff5c
Malo-35 <malo.barbaud@isen.yncrea.fr>
2025-04-25 15:46:32 +0200

    +fonctionnalité [views;home.html] : Ajout de la fonctionnalité de tri des articles en fonction du prix ("neutre", croissant et décroissant) Correction [Dockerfile] : changement de "0.0.0.0:8000" pour "0.0.0.0:8080". Utilitaire [commandes_utiles.txt] : ajout d'un fichier époyme.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import ListView
 from products.models import Product
 
+def RedirectHomeView(request):
+    '''
+    Redirect url from '/' to '/home/'
+    '''
+    return redirect('home')
+
 class HomeView(ListView):
     '''
     Renders home page with all the products, with optional price sorting
